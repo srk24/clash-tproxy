2666bde2bd3e25e77920f0552867abf82a7b5674
Wayne <25836646+srk24@users.noreply.github.com>
2022-03-08 15:56:19 +0800

    fix local

--- local-fake-ip-tproxy-iptables.sh
+++ local-fake-ip-tproxy-iptables.sh
@@ -24,9 +24,6 @@ iptables -t mangle -A TP_CLASH -d 169.254.0.0/16 -j RETURN
 iptables -t mangle -A TP_CLASH -d 224.0.0.0/4 -j RETURN
 iptables -t mangle -A TP_CLASH -d 240.0.0.0/4 -j RETURN
 
-# prevent clash self redirect
-iptables -t mangle -A TP_CLASH -m owner --uid-owner userclash -j RETURN
-
 # prevent dns redirect
 iptables -t mangle -A TP_CLASH -p udp --dport 53 -j RETURN
 
@@ -36,22 +33,62 @@ iptables -t mangle -A TP_CLASH -p udp -j TPROXY --on-port 7893 --tproxy-mark 1
 
 # redirect
 iptables -t mangle -A PREROUTING -j TP_CLASH
-iptables -t mangle -A OUTPUT -j TP_CLASH
 
 # init TP_CLASH_DNS
 iptables -t nat -N TP_CLASH_DNS
 iptables -t nat -F TP_CLASH_DNS
 
 # redirect to clash dns
-iptables -t nat -A TP_CLASH_DNS -m owner --uid-owner userclash -j RETURN
 iptables -t nat -A TP_CLASH_DNS -p udp -j REDIRECT --to-ports 1053
 
 # redirect
 iptables -t nat -A PREROUTING -p udp --dport 53 -j TP_CLASH_DNS
-iptables -t nat -I OUTPUT -p udp --dport 53 -j TP_CLASH_DNS
 
 # DNAT ICMP to local
 iptables -t nat -A PREROUTING -d 198.18.0.0/16 -p icmp -j DNAT --to-destination 127.0.0.1
+
+
+## host network 
+
+# init TP_CLASH_LOCAL
+iptables -t mangle -N TP_CLASH_LOCAL
+iptables -t mangle -F TP_CLASH_LOCAL
+
+# local clients
+iptables -t mangle -A TP_CLASH_LOCAL -d 0.0.0.0/8 -j RETURN
+iptables -t mangle -A TP_CLASH_LOCAL -d 127.0.0.0/8 -j RETURN
+iptables -t mangle -A TP_CLASH_LOCAL -d 10.0.0.0/8 -j RETURN
+iptables -t mangle -A TP_CLASH_LOCAL -d 172.16.0.0/12 -j RETURN
+iptables -t mangle -A TP_CLASH_LOCAL -d 192.168.0.0/16 -j RETURN
+iptables -t mangle -A TP_CLASH_LOCAL -d 169.254.0.0/16 -j RETURN
+iptables -t mangle -A TP_CLASH_LOCAL -d 224.0.0.0/4 -j RETURN
+iptables -t mangle -A TP_CLASH_LOCAL -d 240.0.0.0/4 -j RETURN
+
+# Clash 自己的流量不做处理
+iptables -t mangle -A TP_CLASH_LOCAL -m owner --uid-owner userclash -j RETURN
+
+# 跳过 DNS 流量, 由后面的 NAT 表做 REDIRECT
+iptables -t mangle -A TP_CLASH_LOCAL -p udp --dport 53 -j RETURN
+
+# 其他 TCP/UDP 流量打 MARK 重新路由
+iptables -t mangle -A TP_CLASH_LOCAL -p tcp -j MARK --set-xmark 1
+iptables -t mangle -A TP_CLASH_LOCAL -p udp -j MARK --set-xmark 1
+
+# redirect
+iptables -t mangle -A OUTPUT -j TP_CLASH_LOCAL
+
+# init TP_CLASH_DNS
+iptables -t nat -N TP_CLASH_DNS_LOCAL
+iptables -t nat -F TP_CLASH_DNS_LOCAL
+
+# redirect to clash dns
+iptables -t nat -A TP_CLASH_DNS_LOCAL -m owner --uid-owner userclash -j RETURN
+iptables -t nat -A TP_CLASH_DNS_LOCAL -p udp -j REDIRECT --to-ports 1053
+
+# redirect
+iptables -t nat -A OUTPUT -p udp --dport 53 -j TP_CLASH_DNS_LOCAL
+
+# DNAT ICMP to local
 iptables -t nat -A OUTPUT -d 198.18.0.0/16 -p icmp -j DNAT --to-destination 127.0.0.1
 
 echo "clash iptables init done."
